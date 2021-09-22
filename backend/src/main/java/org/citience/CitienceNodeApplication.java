dfee3b1614147ee6e06bc32ceeeb739944e3d1c5
Philipp Kisters <kisters@informatik.uni-hamburg.de>
2021-09-22 21:34:03 +0200

    init spring backend

---
+++ backend/src/main/java/org/citience/CitienceNodeApplication.java
@@ -0,0 +1,12 @@
+package org.citience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitienceNodeApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CitienceNodeApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/org/citience/CitienceNodeApplicationTest.java
@@ -0,0 +1,14 @@
+package org.citience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CitienceNodeApplicationTest {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
